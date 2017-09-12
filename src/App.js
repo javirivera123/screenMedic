194f0934a5db9be8747f5891205b0972e6580777
Javier Rivera <jrivera17@miners.utep.edu>
2017-09-11 19:23:24 -0600

    adding initial files to git

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react';
+import './App.css'
+import PropTypes from 'prop-types';
+import {BrowserRouter, Route } from 'react-router-dom'
+import 'typeface-roboto'
+import { withStyles } from 'material-ui/styles';
+import HomeView from './views/home.jsx';
+import HoursView from './views/hours.jsx';
+import MySideBar from './components/sidebar.jsx';
+import MyFooter from './components/footer.jsx';
+
+const styles = theme => ({
+    root: {
+        marginTop: 0,
+        width: '100%',
+        backgroundColor:'#fff',
+    },
+    appBar: {
+        height: 60,
+    },
+});
+
+class App extends Component {
+    render() {
+        const classes = this.props.classes;
+        return (
+            <BrowserRouter>
+            <div className={classes.root}>
+                <MySideBar>
+                </MySideBar>
+                <Route exact path='/' render={() => <HomeView> </HomeView>} />
+                <Route
+                    exact
+                    path='/hours'
+                    render={() => <HoursView> </HoursView>}
+                />
+                <Route
+                    exact
+                    path='/call'
+                    render={() => <h1>  Call </h1>}
+                />
+                <Route
+                    exact
+                    path='/reviews'
+                    render={() => <h1> Reviews </h1>}
+                />
+                <Route
+                    exact
+                    path='/mail'
+                    render={() => <h1> Mail </h1>}
+                />
+                <Route
+                    exact
+                    path='/directions'
+                    render={() => <h1> Directions </h1>}
+                />
+                <MyFooter>
+                </MyFooter>
+            </div>
+        </BrowserRouter>
+        );
+    }
+}
+
+App.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(App);
---
+++ src/components/footer.jsx
@@ -0,0 +1,35 @@
+// @flow weak
+
+import React from 'react';
+import PropTypes from 'prop-types';
+import 'typeface-roboto'
+import { withStyles } from 'material-ui/styles';
+import { GridList, GridListTile } from 'material-ui/GridList';
+
+const styles = theme => ({
+    root: {
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        backgroundColor:'#24292e',
+        color:'#fff',
+        textAlign: 'center',
+    },
+});
+
+function Footer(props) {
+    const classes = props.classes;
+
+    return (
+        <div className={classes.root}>
+            <h5>  Headquarters Hair & Apperal 145. S. Mesa Hills El Paso TX 79902 </h5>
+        </div>
+    );
+}
+
+Footer.propTypes = {
+    classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Footer);

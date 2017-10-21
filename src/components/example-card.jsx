48fb71d1e943d6de2d8f76ea4f7b939a08827688
Javier Rivera <jrivera17@miners.utep.edu>
2017-10-20 18:26:01 -0600

    changed to 3 columns on homepage

--- src/components/example-card.jsx
+++ src/components/example-card.jsx
@@ -5,6 +5,7 @@ import Card, { CardActions, CardContent, CardMedia } from 'material-ui/Card';
 import Button from 'material-ui/Button';
 import Typography from 'material-ui/Typography';
 
+
 const styles = {
     card: {
         maxWidth: 345,
@@ -35,7 +36,7 @@ class SimpleMediaCard extends React.Component {
                             {this.props.title}
                         </Typography>
                         <Typography component="p">
-                            {this.props.desc}
+
                         </Typography>
                     </CardContent>
                     <CardActions>
--- src/views/home.jsx
+++ src/views/home.jsx
@@ -8,6 +8,8 @@ import Typography from 'material-ui/Typography';
 import ExampleCard from '../components/example-card.jsx';
 import {Grid} from "../../node_modules/material-ui/index";
 
+let MediaReact = require('react-media');
+
 const styles = theme => ({
     root: {
         flexGrow: 1,
@@ -24,8 +26,16 @@ const styles = theme => ({
         backgroundSize: 'cover',
     },
     paper: theme.mixins.gutters({
-        paddingTop: 8,
+        paddingTop: 10,
         paddingBottom: 16,
+        marginTop: theme.spacing.unit * 6,
+        marginBottom: theme.spacing.unit * 10,
+        marginLeft: theme.spacing.unit * 8,
+        marginRight: theme.spacing.unit * 8,
+    }),
+    mobilePaper: theme.mixins.gutters({
+        paddingTop: 8,
+        paddingBottom: 8,
         marginTop: theme.spacing.unit * 3,
         marginBottom: theme.spacing.unit * 3,
         marginLeft: theme.spacing.unit * 3,
@@ -49,55 +59,102 @@ class Home extends Component {
         const classes = this.props.classes;
         return(
             <div>
-                <Paper className={classes.paper} elevation={10}>
-                    <CardMedia
-                        className={classes.media}
-                        image={process.env.PUBLIC_URL + 'banner/mountains.jpg'}
-                        title="Contemplativ"
-                    />
+
                     <div>
-                        <h1> Screen Medic: SmartPhone Repair  </h1>
+                        <MediaReact query="(max-width: 599px)">
+                            {matches => matches ? (
+                                    <div>
+                                        <Paper className={classes.mobilePaper} elevation={10}>
+                                            <CardMedia
+                                                className={classes.media}
+                                                image={process.env.PUBLIC_URL + 'banner/mountains.jpg'}
+                                                title="Contemplativ"
+                                            />
+                                            <div>
+                                                <h1> Screen Medic: SmartPhone Repair  </h1>
+                                            </div>
+                                            <h4>   </h4>
+                                            <Typography type="body1" component="p">
+                                                Got a cracked or broken smartphone device? Look no further! The Screen Medic can have your
+                                                device back to great condition!
+                                            </Typography>
+
+                                            <div className={classes.mobile}>
+                                                <Grid container spacing={10}>
+                                                    <Grid item xs={12}>
+                                                        <ExampleCard
+                                                            title = {'iphone X'}
+                                                            image = {process.env.PUBLIC_URL + './iphonepics/ip7.png'}>
+                                                            price = {'Coming Soon'}
+                                                        </ExampleCard>
+                                                    </Grid>
+                                                    <Grid item xs={12}>
+                                                        <ExampleCard
+                                                            title = {'iphone '}
+                                                            image = {process.env.PUBLIC_URL + './iphonepics/iphone6.png'}>
+                                                        </ExampleCard>
+                                                    </Grid>
+                                                    <Grid item xs={12}>
+                                                        <ExampleCard
+                                                            title = {'iphone 5'}
+                                                            image = {process.env.PUBLIC_URL + './iphonepics/ip7.png'}>
+                                                        </ExampleCard>
+                                                    </Grid>
+                                                    <Grid item xs={12} sm={6}>
+                                                        <ExampleCard
+                                                            title = {'iPad'}
+                                                            image = {process.env.PUBLIC_URL + './iphonepics/iphone6.png'}>
+                                                        </ExampleCard>
+                                                    </Grid>
+                                                </Grid>
+                                            </div>
+                                        </Paper>
+                                    </div>
+                            ) : (
+                                <div>
+                                    <Paper className={classes.paper} elevation={10}>
+                                        <CardMedia
+                                            className={classes.media}
+                                            image={process.env.PUBLIC_URL + 'banner/mountains.jpg'}
+                                            title="Contemplativ"
+                                        />
+                                        <div>
+                                            <h1> Screen Medic: SmartPhone Repair  </h1>
+                                        </div>
+                                        <h4>   </h4>
+                                        <Typography type="body1" component="p">
+                                            Got a cracked or broken smartphone device? Look no further! The Screen Medic can have your
+                                            device back to great condition!
+                                        </Typography>
+                                            <div className={classes.root}>
+                                                <Grid container spacing={24}>
+                                                    <Grid item xs>
+                                                        <ExampleCard
+                                                            title = {'iphone 7'}
+                                                            image = {process.env.PUBLIC_URL + './iphonepics/ip7.png'}>
+                                                            price = {'$89'}
+                                                        </ExampleCard>
+                                                    </Grid>
+                                                    <Grid item xs>
+                                                        <ExampleCard
+                                                            title = {'iphone 6'}
+                                                            image = {process.env.PUBLIC_URL + './iphonepics/iphone6.png'}>
+                                                        </ExampleCard>
+                                                    </Grid>
+                                                    <Grid item xs>
+                                                        <ExampleCard
+                                                            title = {'iphone 5'}
+                                                            image = {process.env.PUBLIC_URL + './iphonepics/ip7.png'}>
+                                                        </ExampleCard>
+                                                    </Grid>
+                                                </Grid>
+                                            </div>
+                                    </Paper>
+                                </div>
+                            )}
+                        </MediaReact>
+
                     </div>
-                    <h4>   </h4>
-                    <Typography type="body1" component="p">
-                        Got a cracked or broken smartphone device? Look no further! The Screen Medic can have your
-                        device back to great condition!
-                    </Typography>
-                    <div>
-                        <div className={classes.root}>
-                            <Grid container spacing={24}>
-                                <Grid item xs={12} sm={6}>
-                                    <ExampleCard
-                                        title = {'iphone 7'}
-                                        image = {process.env.PUBLIC_URL + './iphonepics/ip7.png'}
-                                        desc = "lorem ipsum">
-                                    </ExampleCard>
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <ExampleCard
-                                        title = {'iphone 6'}
-                                        image = {process.env.PUBLIC_URL + './iphonepics/iphone6.png'}>
-                                    </ExampleCard>
-                                </Grid>
-                            </Grid>
-                            <Grid container spacing={24}>
-                                <Grid item xs={12} sm={6}>
-                                    <ExampleCard
-                                        title = {'iphone 7'}
-                                        image = {process.env.PUBLIC_URL + './iphonepics/ip7.png'}
-                                        desc = "lorem ipsum">
-                                    </ExampleCard>
-                                </Grid>
-                                <Grid item xs={12} sm={6}>
-                                    <ExampleCard
-                                        title = {'iphone 6'}
-                                        image = {process.env.PUBLIC_URL + './iphonepics/iphone6.png'}>
-                                    </ExampleCard>
-                                </Grid>
-                            </Grid>
-                        </div>
-                    </div>
-                </Paper>
             </div>
         );
     }
